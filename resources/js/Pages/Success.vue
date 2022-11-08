83384cf22be848617b5c6c4323db780a667f1bf1
Jonathan <jonathan.oeijoeng@gmail.com>
2022-11-08 11:12:17 +0700

    Add Midtrans Payment Gateway

---
+++ resources/js/Pages/Success.vue
@@ -0,0 +1,29 @@
+<template>
+    <AppLayout>
+        {{ data.data }}
+    </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { Inertia } from "@inertiajs/inertia";
+import Swal from "sweetalert2";
+
+const data = defineProps({
+    data: Object,
+});
+
+// Swal.fire({
+//     title: "<strong>Success!</strong>",
+//     text: "Payment Success",
+//     icon: "success",
+//     showCancelButton: false,
+//     confirmButtonColor: "#3085d6",
+//     cancelButtonColor: "#d33",
+//     confirmButtonText: "OK",
+// }).then((result) => {
+//     if (result.isConfirmed) {
+//         Inertia.get(route("table"));
+//     }
+// });
+</script>
